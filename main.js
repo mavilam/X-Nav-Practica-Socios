3d4a3ae61988e1f45c3c7e40ba2dae1e415588a6
Miguel Ávila Monroy <soyer893@gmail.com>
2015-03-25 23:24:01 +0100

    Añadido boton de abrir

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ $(document).ready(function() {
     		var div = "#"+i+"timeline";
 	       $("#timelinemsg").append("<div class=\"alert alert-danger \" id=\"" + i +"timeline\" role=\"alert\"></div>");
 	       var inner = "<div class ='container'><div class='row'><div class='col-md-9'>";
-				inner += "<h3>"+item["Author"]+"</h3><h4> "+item["Title"]+"</h4></br><p> "+item["Content"]+"</p></div>";
+				inner += "<h3>"+item["Author"]+"</h3><h4> "+item["Title"]+"</h4></br><button type='button' data-toggle='collapse' ";
+				inner += "data-target='#all"+i+"' class='btn btn-danger'>Abrir</button>";
+				inner += "<div class='collapse' id='all"+i+"'><div class='well'><p> "+item["Content"]+"</p></div></div></div>";
 				inner += "<div class='col-md-2'><img class='Avatar' src='"+item["Avatar"]+"'></img>";
 				inner += "<p>Date: "+item["Date"]+"</p>"
 				inner += "</div></div></div></div></br>";
@@ -35,7 +37,9 @@ $(document).ready(function() {
     		var div = "#"+i+"myline";
 	       $("#mylinemsg").append("<div class=\"alert alert-danger \" id=\"" + i +"myline\" role=\"alert\"></div>");
 	       var inner = "<div class ='container'><div class='row'><div class='col-md-9'>";
-				inner += "<h3>"+item["Author"]+"</h3><h4> "+item["Title"]+"</h4></br><p> "+item["Content"]+"</p></div>";
+				inner += "<h3>"+item["Author"]+"</h3><h4> "+item["Title"]+"</h4></br><button type='button' data-toggle='collapse' ";
+				inner += "data-target='#mylineall"+i+"' class='btn btn-danger'>Abrir</button>";
+				inner += "<div class='collapse' id='mylineall"+i+"'><div class='well'><p> "+item["Content"]+"</p></div></div></div>";
 				inner += "<div class='col-md-2'><img class='Avatar' src='"+item["Avatar"]+"'></img>";
 				inner += "<p>Date: "+item["Date"]+"</p>"
 				inner += "</div></div></div></div></br>";
@@ -50,7 +54,9 @@ $(document).ready(function() {
     		var div = "#"+i+"updatetimeline";
 	       $("#updatetimeline").append("<div class=\"alert alert-danger \" id=\"" + i +"updatetimeline\" role=\"alert\"></div>");
 	       var inner = "<div class ='container'><div class='row'><div class='col-md-9'>";
-				inner += "<h3>"+item["Author"]+"</h3><h4> "+item["Title"]+"</h4></br><p> "+item["Content"]+"</p></div>";
+				inner += "<h3>"+item["Author"]+"</h3><h4> "+item["Title"]+"</h4></br><button type='button' data-toggle='collapse' ";
+				inner += "data-target='#newall"+i+"' class='btn btn-danger'>Abrir</button>";
+				inner += "<div class='collapse' id='newall"+i+"'><div class='well'><p> "+item["Content"]+"</p></div></div></div>";
 				inner += "<div class='col-md-2'><img class='Avatar' src='"+item["Avatar"]+"'></img>";
 				inner += "<p>Date: "+item["Date"]+"</p>"
 				inner += "</div></div></div></div></br>";
